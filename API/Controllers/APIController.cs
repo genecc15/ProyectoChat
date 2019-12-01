25033f51768c3f253a5c928edff9f663e46343f5
genecc15 <genecc15@gmail.com>
2019-12-01 17:55:34 -0600

    Api Usuarios

--- API/Controllers/APIController.cs
+++ API/Controllers/APIController.cs
@@ -11,11 +11,63 @@ namespace API.Controllers
     {
         private readonly UsuariosServices _UsuariosService;
 
-        public UsuariosController(UsuariosServices usuariosServices)
+        public APIController(UsuariosServices usuariosServices)
         {
             _UsuariosService = usuariosServices;
         }
 
+        //http://localhost:<port>/API/Usuarios
+        [HttpGet]
+        public ActionResult<List<Usuarios>> Get() =>
+            _UsuariosService.Get();
 
+        //http://localhost:<port>/API/Usuarios/id
+        [HttpGet("{id: length(24)", Name = "GetUsuarios")]
+        public ActionResult<Usuarios> Get(string id)
+        {
+            var Usuarios = _UsuariosService.Get(id);
+            if(Usuarios==null)
+            {
+                return NotFound();
+            }
+            return Usuarios;
+        }
+        [HttpPost]
+        public ActionResult<Usuarios> Create(Usuarios usuarios)
+        {
+            _UsuariosService.Create(usuarios);
+            return CreatedAtRoute("GetUsuarios", new { id = usuarios.Id.ToString()}, usuarios);
+                
+        }
+
+        //http://localhost<port>/API/Usuarios/id
+
+        [HttpPut("{id: length(24)}")]
+
+        public IActionResult Update(string id, Usuarios UsuariosIn)
+        {
+            var Usuario = _UsuariosService.Get(id);
+
+            if(Usuario==null)
+            {
+                return NotFound();
+            }
+
+            _UsuariosService.Update(id, UsuariosIn);
+            return Ok();
+        }
+        //http://localhost<port>/API/Usuarios/id
+        [HttpDelete("{id: length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var Usuario = _UsuariosService.Get(id);
+
+            if(Usuario==null)
+            {
+                return NotFound();
+            }
+            _UsuariosService.Remove(Usuario.Id);
+            return NoContent();
+        }
     }
 }

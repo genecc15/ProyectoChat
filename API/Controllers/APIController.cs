67588a875e985a12f2f02f2323c35eb04d520cc0
genecc15 <genecc15@gmail.com>
2019-12-01 17:36:43 -0600

    API

---
+++ API/Controllers/APIController.cs
@@ -0,0 +1,21 @@
+﻿using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        private readonly UsuariosServices _UsuariosService;
+
+        public UsuariosController(UsuariosServices usuariosServices)
+        {
+            _UsuariosService = usuariosServices;
+        }
+
+
+    }
+}
